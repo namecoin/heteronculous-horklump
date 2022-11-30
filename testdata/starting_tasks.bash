24fe051c968573a2297bdb571272656401c63b72
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-11-30 14:08:39 +0300

    yml fix

--- testdata/starting_tasks.bash
+++ testdata/starting_tasks.bash
@@ -5,7 +5,7 @@ shopt -s nullglob globstar
 
 git clone -b starting_tasks https://github.com/namecoin/heteronculous-horklump && cd heteronculous-horklump
 cd Starting_Tasks
-cd "$TASK_NUMBER" && cd Testing_application
+cd "$TASK_NUMBER"/Testing_application
 export GOBIN="$PWD" && go install hello.go
 cd ..
 
