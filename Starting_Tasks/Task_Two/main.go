ae585c213277106ee7bac210b9a945f3bdfe7b90
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-12-02 22:02:37 +0300

    Socket SysCalls Func

--- Starting_Tasks/Task_Two/main.go
+++ Starting_Tasks/Task_Two/main.go
@@ -7,21 +7,26 @@ import (
 )
 
 func main() {
-	// Socket call functions
-	socketfunctions := map[string]struct{}{"socket":{},"bind":{},"connect":{},"listen":{},"accept":{},"getsockname":{},
-	"getpeername":{},"socketpair":{},"send":{},"recv":{},"sendto":{},"recvfrom":{},"shutdown":{},"setsockopt":{},
-	"getsockopt":{},"sendmsg":{},"recvmsg":{},"accept4":{},"recvmmsg":{},"sendmmsg":{}}
-	
-	if err := strace.Trace(exec.Command("ping","google.com"), func(t strace.Task, record *strace.TraceRecord) error {
-		// Getting the name of the Socket System Call.
-		SyscallName, _ := strace.ByNumber(uintptr(record.Syscall.Sysno))
-		if _, err := socketfunctions[SyscallName]; !err {
-			return nil
-		}
-
-		fmt.Printf("Detected a Socket System Call: %v\n", SyscallName)
-		return nil
+	// Start the program with tracing.
+	if err := strace.Trace(exec.Command("ping","google.com"), func(t strace.Task, record *strace.TraceRecord) error{
+		return SocketSysCalls(record)			
 	}); err != nil {
 		panic(err)
 	}
 }
+// SocketSysCalls checks if a syscall is a socket syscall.
+func SocketSysCalls(r *strace.TraceRecord) error{
+	// Socket call functions from Ubuntu Manuals (https://manpages.ubuntu.com/manpages/bionic/man2/socketcall.2.html)
+	socketfunctions := map[string]struct{}{"socket":{},"bind":{},"connect":{},"listen":{},"accept":{},"getsockname":{},
+	"getpeername":{},"socketpair":{},"send":{},"recv":{},"sendto":{},"recvfrom":{},"shutdown":{},"setsockopt":{},
+	"getsockopt":{},"sendmsg":{},"recvmsg":{},"accept4":{},"recvmmsg":{},"sendmmsg":{}}
+
+	// Getting the name of the Socket System Call.
+	SyscallName, _ := strace.ByNumber(uintptr(r.Syscall.Sysno))
+	if _, err := socketfunctions[SyscallName]; !err {
+		return nil
+	}
+	fmt.Printf("Detected a Socket System Call: %v\n", SyscallName)
+	return nil
+}
+ 
