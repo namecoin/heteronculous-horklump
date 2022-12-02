a26d0386a1e58423fa4c00f89f80d493c5cd8a9e
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-12-02 14:56:28 +0300

    FIX

--- Starting_Tasks/Task_Two/main.go
+++ Starting_Tasks/Task_Two/main.go
@@ -7,17 +7,19 @@ import (
 )
 
 func main() {
+	// Socket call functions
 	socketfunctions := map[string]struct{}{"socket":{},"bind":{},"connect":{},"listen":{},"accept":{},"getsockname":{},
 	"getpeername":{},"socketpair":{},"send":{},"recv":{},"sendto":{},"recvfrom":{},"shutdown":{},"setsockopt":{},
 	"getsockopt":{},"sendmsg":{},"recvmsg":{},"accept4":{},"recvmmsg":{},"sendmmsg":{}}
 	
 	if err := strace.Trace(exec.Command("ping","google.com"), func(t strace.Task, record *strace.TraceRecord) error {
+		// Getting the name of the Socket System Call.
 		SyscallName, _ := strace.ByNumber(uintptr(record.Syscall.Sysno))
 		if _, err := socketfunctions[SyscallName]; !err {
 			return nil
 		}
 
-		fmt.Printf("Detect a Socket System Call: %v", SyscallName)
+		fmt.Printf("Detected a Socket System Call: %v", SyscallName)
 		return nil
 	}); err != nil {
 		panic(err)
