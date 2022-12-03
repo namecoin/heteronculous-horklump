8057795e518a80f8446e649c7c8f09719c3c4fc3
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2022-12-03 20:59:29 +0300

    Socket SysCalls Func

--- testdata/tasktwo.bash
+++ testdata/tasktwo.bash
@@ -3,12 +3,17 @@
 set -euo pipefail
 shopt -s nullglob globstar
 
+Check() {
+    if grep -i "socket" dunp.txt; then
+        exit 0
+    else
+        exit 1
+}
+
 git clone -b starting_tasks "$CIRRUS_REPO_CLONE_URL"
 cd heteronculous-horklump/Starting_Tasks/Task_Two
 
+timeout 10 go run main.go >> dump.txt ; Check
 
-if timeout 10 go run main.go; then
-    exit 1
-else
-    exit 0
-fi
+
+
